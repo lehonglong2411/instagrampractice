96a964630b4461bffb89c503a7e41602330a474a
Hong Long <lehonglong2411@gmail.com>
2022-04-26 19:59:06 +0700

    fix bug complete 30:38

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
+
+import { initializeApp } from 'firebase/app';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+};
+
+initializeApp(firebaseConfig);
 
 const Stack = createStackNavigator();
 export default function App() {
